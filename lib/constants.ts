07b286044e3def64cddf2c14c67adf92282a09bc
ogzhanolguncu <ogzhan11@gmail.com>
2022-01-18 01:12:46 +0300

    Remove fakerjs add randomInRange

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,93 +1,69 @@
-import faker from "@faker-js/faker";
 import { nanoid } from "nanoid";
+import { getRandomInRange } from "utils";
 
 export const mockBookData = [
 	{
 		id: nanoid(),
 		name: "1984",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "George Orwell",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: false,
 	},
 	{
 		id: nanoid(),
 		name: "Brave New World",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "Albert Huxley",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: false,
 	},
 	{
 		id: nanoid(),
 		name: "The Lord of The Rings",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "Tolkein",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: true,
 	},
 	{
 		id: nanoid(),
 		name: "Utopia",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "John Moore",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: false,
 	},
 	{
 		id: nanoid(),
 		name: "East of Eden",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "Oguzhan",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: false,
 	},
 	{
 		id: nanoid(),
 		name: "A Scanner Darkly",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "TODO",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: true,
 	},
 	{
 		id: nanoid(),
 		name: "East of Eden",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "TODO",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: false,
 	},
 	{
 		id: nanoid(),
 		name: "A Scanner Darkly",
 		image: "https://via.placeholder.com/300",
-		author: faker.name.firstName(),
-		pageNumber: faker.datatype.number({
-			min: 70,
-			max: 250,
-		}),
+		author: "TODO",
+		pageNumber: getRandomInRange(70, 250),
 		isCompleted: true,
 	},
 ];
