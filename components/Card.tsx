5375f4d85634fc73c742c3d0c5b6413263961ef9
Oguzhan Olguncu <oguzhan@fireflyon.com>
2022-01-18 17:03:20 +0300

    Fix spacing issues

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,54 +2,62 @@ import React, { FC } from "react";
 import { Flex, VStack, Image, Text, Box } from "@chakra-ui/react";
 
 type Props = {
-	book: Livre.Book;
+  book: Livre.Book;
 };
 
-const Card: FC<Props> = ({ book }) => {
-	return (
-		<Flex flexDirection="column" key={book.id}>
-			<Box position="relative" width="300px" height="300px">
-				<Flex
-					transition="all 0.5s ease"
-					boxShadow="20px 38px 34px -26px rgb(0 0 0 / 20%)"
-					borderRadius="md"
-					width="300px"
-					backgroundColor="#3d2c8d8c"
-					border="3px solid #e76bde"
-					height="300px"
-					alt="cover"
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Text fontSize="5xl" textAlign="center">
-						{book.name}
-					</Text>
-				</Flex>
-				{!book.isCompleted && (
-					<Text
-						position="absolute"
-						bottom="0"
-						background="rgb(15 14 14 / 29%)"
-						color="#f1f1f1"
-						width="100%"
-						padding="1rem"
-						fontSize="3xl"
-						borderBottomRightRadius="md"
-						borderBottomLeftRadius="md"
-						cursor="pointer"
-					>
-						Add To Lib
-					</Text>
-				)}
-			</Box>
+const cardBreakPoints = ["100px", "240px", "240px", "300px"];
+const fontSizeBreakPoints = ["xl", "2xl", "2xl", "3xl"];
+const bookNameBreakPoints = ["2xl", "4xl", "4xl", "5xl"];
 
-			<VStack align="flex-start" px="0.5rem" mt="1.5rem" maxWidth="300px">
-				<Text fontSize="3xl">Author: {book.author}</Text>
-				<Text fontSize="3xl">Page Number: {book.pageNumber}</Text>
-				<Text fontSize="3xl">Completion: {book.isCompleted ? "✔️" : "⌛"}</Text>
-			</VStack>
-		</Flex>
-	);
+const Card: FC<Props> = ({ book }) => {
+  return (
+    <Flex flexDirection="column" key={book.id}>
+      <Box position="relative" width={cardBreakPoints} height={cardBreakPoints}>
+        <Flex
+          transition="all 0.5s ease"
+          boxShadow="20px 38px 34px -26px rgb(0 0 0 / 20%)"
+          borderRadius="md"
+          width={cardBreakPoints}
+          backgroundColor="#3d2c8d8c"
+          border="3px solid #e76bde"
+          height={cardBreakPoints}
+          alt="cover"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text fontSize={bookNameBreakPoints} textAlign="center">
+            {book.name}
+          </Text>
+        </Flex>
+        {!book.isCompleted && (
+          <Text
+            position="absolute"
+            bottom="0"
+            background="rgb(15 14 14 / 29%)"
+            color="#f1f1f1"
+            width="100%"
+            padding="1rem"
+            borderBottomRightRadius="md"
+            borderBottomLeftRadius="md"
+            cursor="pointer"
+            fontSize={fontSizeBreakPoints}
+          >
+            Add To Lib
+          </Text>
+        )}
+      </Box>
+
+      <VStack align="flex-start" px="0.5rem" mt="1.5rem" maxWidth="300px">
+        <Text fontSize={fontSizeBreakPoints}>Author: {book.author}</Text>
+        <Text fontSize={fontSizeBreakPoints}>
+          Page Number: {book.pageNumber}
+        </Text>
+        <Text fontSize={fontSizeBreakPoints}>
+          Completion: {book.isCompleted ? "✔️" : "⌛"}
+        </Text>
+      </VStack>
+    </Flex>
+  );
 };
 
 export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,22 +9,23 @@ import type { NextPage } from "next";
 //TODO: Connect to supabase
 
 const Home: NextPage = () => {
-	return (
-		<Flex height="100vh">
-			<Sidebar />
-			<Flex flexDir="column" justifyContent="center" alignItems="center" width="100%">
-				<Grid
-					templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]}
-					columnGap="7rem"
-					rowGap="3rem"
-				>
-					{mockBookData.map((book) => (
-						<Card book={book} key={book.id} />
-					))}
-				</Grid>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex>
+      <Sidebar />
+      <Flex
+        width="100%"
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        gap="3rem"
+        my="2.5rem"
+      >
+        {mockBookData.map((book) => (
+          <Card book={book} key={book.id} />
+        ))}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
