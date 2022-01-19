a4230b510088d94b2dcd91bdd47655b9b1d37e5d
Mustafa Yeşilbaş <mustafayesilbas@192.168.1.103>
2022-01-19 22:26:05 +0300

    fix sidebar button and text size

--- components/IconContainer.tsx
+++ components/IconContainer.tsx
@@ -9,18 +9,24 @@ type Props = {
 	showTexts: boolean;
 };
 
+const showTextFontSizeBreakPoints = ["2xl", "2xl", "3xl", "4xl"] 
+const noShowTextFontSizeBreakPoints = ["xl","2xl","3xl","4xl"] 
+const showTextDisplayBreakPoints = ["none", "none", "none", "block"]
+const noShowTextDisplayBreakPoints = ["block", "block", "block", "none"]
+const fontSizeBreakPoints = ["2xl", "2xl", "4xl", "4xl"]
+
 const IconContainer = ({ text, icon, showTexts, href }: Props) => {
+	const test=""
+	console.warn("showTexts",showTexts)
 	return (
 		<Flex>
-			<Text fontSize={["2xl", "2xl", "4xl", "4xl"]}>
+			<Text fontSize={fontSizeBreakPoints}>
 				<Link href={href}>{icon}</Link>
 			</Text>
 
 			<Text
-				fontSize={["2xl", "2xl", "3xl", "4xl"]}
-				display={
-					showTexts ? ["block", "block", "block", "block"] : ["none", "none", "none", "block"]
-				}
+				fontSize={ showTexts ? showTextFontSizeBreakPoints : noShowTextFontSizeBreakPoints } 
+				display={ showTexts ? showTextDisplayBreakPoints : noShowTextDisplayBreakPoints }
 			>
 				<Link href={href}>{text}</Link>
 			</Text>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,44 +3,48 @@ import { Flex, VStack, Text, useBoolean } from "@chakra-ui/react";
 import IconContainer from "./IconContainer";
 
 const Sidebar = () => {
-	const [flag, setFlag] = useBoolean(true);
+  const [flag, setFlag] = useBoolean(true);
 
-	return (
-		<Flex
-			height="100vh"
-			backgroundColor="#3d2c8d8c"
-			width={["50%", "30%", "40%", "15%"]}
-			flexDir="column"
-			justifyContent="flex-start"
-			alignItems="center"
-			position="sticky"
-			top="0"
-			borderRight="3px solid #916BBF"
-		>
-			<Flex marginTop="3rem" flexDir="column">
-				<VStack
-					align={[`${flag ? "flex-start" : "center"}`, "center", "flex-start", "flex-start"]}
-					px="1rem"
-					gap="3"
-				>
-					<Text
-						width="100%"
-						textAlign="center"
-						fontSize="4xl"
-						cursor="pointer"
-						onClick={setFlag.toggle}
-						display={["block", "block", "block", "none"]}
-					>
-						🍔
-					</Text>
+  return (
+    <Flex
+      height="100vh"
+      backgroundColor="#3d2c8d8c"
+      width={flag ? ["20%", "15%", "20%", "20%"] : ["55%", "30%", "30%", "25%"]}
+      flexDir="column"
+      justifyContent="flex-start"
+      alignItems="center"
+      position="sticky"
+      top="0"
+      borderRight="3px solid #916BBF"
+    >
+      <Flex marginTop="3rem" flexDir="column">
+        <VStack
+          alignItems={
+            flag
+              ? ["center", "center", "center", "flex-start"]
+              : ["flex-start", "flex-start", "flex-start", "flex-start"]
+          }
+          px="1rem"
+          gap="3"
+        >
+          <Text
+            width="100%"
+            textAlign="center"
+            fontSize="4xl"
+            cursor="pointer"
+            onClick={setFlag.toggle}
+            display={["block", "block", "block", "none"]}
+          >
+            🍔
+          </Text>
 
-					<IconContainer icon="🔐" text="Sign-in" showTexts={flag} href="#" />
-					<IconContainer icon="☄️" text="Join" showTexts={flag} href="#" />
-					<IconContainer icon="📚" text="My Books" showTexts={flag} href="#" />
-				</VStack>
-			</Flex>
-		</Flex>
-	);
+          <IconContainer icon="🔐" text="Sign-in" showTexts={flag} href="#" />
+          <IconContainer icon="☄️" text="Join" showTexts={flag} href="#" />
+          <IconContainer icon="📚" text="My Books" showTexts={flag} href="#" />
+        </VStack>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Sidebar;
