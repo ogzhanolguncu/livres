20b435fbffb6f52eabe584d9e4fe0008d33443a4
ogzhanolguncu <ogzhan11@gmail.com>
2022-01-19 22:39:36 +0300

    Add icons to sidebar

--- components/IconContainer.tsx
+++ components/IconContainer.tsx
@@ -1,32 +1,35 @@
 import Link from "next/link";
 import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Icon, Text } from "@chakra-ui/react";
+
+import type { IconType } from "react-icons";
 
 type Props = {
 	text: string;
-	icon: string;
+	icon: IconType;
 	href: string;
 	showTexts: boolean;
 };
 
-const showTextFontSizeBreakPoints = ["2xl", "2xl", "3xl", "4xl"] 
-const noShowTextFontSizeBreakPoints = ["xl","2xl","3xl","4xl"] 
-const showTextDisplayBreakPoints = ["none", "none", "none", "block"]
-const noShowTextDisplayBreakPoints = ["block", "block", "block", "none"]
-const fontSizeBreakPoints = ["2xl", "2xl", "4xl", "4xl"]
+const showTextFontSizeBreakPoints = ["2xl", "2xl", "3xl", "4xl"];
+const noShowTextFontSizeBreakPoints = ["xl", "2xl", "3xl", "4xl"];
+const showTextDisplayBreakPoints = ["none", "none", "none", "block"];
+const noShowTextDisplayBreakPoints = ["block", "block", "block", "none"];
+const fontSizeBreakPoints = ["2xl", "2xl", "4xl", "4xl"];
 
 const IconContainer = ({ text, icon, showTexts, href }: Props) => {
-	const test=""
-	console.warn("showTexts",showTexts)
 	return (
 		<Flex>
 			<Text fontSize={fontSizeBreakPoints}>
-				<Link href={href}>{icon}</Link>
+				<Link href={href} passHref>
+					<Icon as={icon} w={12} h={12} />
+				</Link>
 			</Text>
 
 			<Text
-				fontSize={ showTexts ? showTextFontSizeBreakPoints : noShowTextFontSizeBreakPoints } 
-				display={ showTexts ? showTextDisplayBreakPoints : noShowTextDisplayBreakPoints }
+				marginLeft="1rem"
+				fontSize={showTexts ? showTextFontSizeBreakPoints : noShowTextFontSizeBreakPoints}
+				display={showTexts ? showTextDisplayBreakPoints : noShowTextDisplayBreakPoints}
 			>
 				<Link href={href}>{text}</Link>
 			</Text>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,50 +1,52 @@
 import React from "react";
-import { Flex, VStack, Text, useBoolean } from "@chakra-ui/react";
+import { Flex, VStack, Text, useBoolean, Icon } from "@chakra-ui/react";
 import IconContainer from "./IconContainer";
+import { FaSignInAlt, FaMeteor, FaHamburger } from "react-icons/fa";
+import { GiBookshelf } from "react-icons/gi";
 
 const Sidebar = () => {
-  const [flag, setFlag] = useBoolean(true);
+	const [flag, setFlag] = useBoolean(true);
 
-  return (
-    <Flex
-      height="100vh"
-      backgroundColor="#3d2c8d8c"
-      width={flag ? ["20%", "15%", "20%", "20%"] : ["55%", "30%", "30%", "25%"]}
-      flexDir="column"
-      justifyContent="flex-start"
-      alignItems="center"
-      position="sticky"
-      top="0"
-      borderRight="3px solid #916BBF"
-    >
-      <Flex marginTop="3rem" flexDir="column">
-        <VStack
-          alignItems={
-            flag
-              ? ["center", "center", "center", "flex-start"]
-              : ["flex-start", "flex-start", "flex-start", "flex-start"]
-          }
-          px="1rem"
-          gap="3"
-        >
-          <Text
-            width="100%"
-            textAlign="center"
-            fontSize="4xl"
-            cursor="pointer"
-            onClick={setFlag.toggle}
-            display={["block", "block", "block", "none"]}
-          >
-            🍔
-          </Text>
+	return (
+		<Flex
+			height="100vh"
+			backgroundColor="#3d2c8d8c"
+			width={flag ? ["20%", "15%", "20%", "10%"] : ["55%", "30%", "30%", "25%"]}
+			flexDir="column"
+			justifyContent="flex-start"
+			alignItems="center"
+			position="sticky"
+			top="0"
+			borderRight="3px solid #916BBF"
+		>
+			<Flex marginTop="2rem" flexDir="column">
+				<VStack
+					alignItems={
+						flag
+							? ["center", "center", "center", "flex-start"]
+							: ["flex-start", "flex-start", "flex-start", "flex-start"]
+					}
+					px="1rem"
+					gap="3"
+				>
+					<Text
+						width="100%"
+						textAlign="center"
+						fontSize="4xl"
+						cursor="pointer"
+						onClick={setFlag.toggle}
+						display={["block", "block", "block", "none"]}
+					>
+						<Icon as={FaHamburger} w={12} h={12} />
+					</Text>
 
-          <IconContainer icon="🔐" text="Sign-in" showTexts={flag} href="#" />
-          <IconContainer icon="☄️" text="Join" showTexts={flag} href="#" />
-          <IconContainer icon="📚" text="My Books" showTexts={flag} href="#" />
-        </VStack>
-      </Flex>
-    </Flex>
-  );
+					<IconContainer icon={FaSignInAlt} text="Sign-in" showTexts={flag} href="#" />
+					<IconContainer icon={FaMeteor} text="Join" showTexts={flag} href="#" />
+					<IconContainer icon={GiBookshelf} text="My Books" showTexts={flag} href="#" />
+				</VStack>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Sidebar;
