545868930cd1987bcd58370f782757495e37e53a
ogzhanolguncu <ogzhan11@gmail.com>
2022-01-20 23:34:41 +0300

    Add yup for validation

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,117 +1,93 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Input,
-  Text,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from "@chakra-ui/react";
-import Sidebar from "@components/Sidebar";
-import { supabase } from "lib/supabaseClient";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { Box, Button, Flex, Heading, Input, Text } from "@chakra-ui/react";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
+import { supabase } from "lib/supabaseClient";
+
 const loginSchema = yup
-  .object({
-    email: yup.string().email().required(),
-  })
-  .required();
+	.object({
+		email: yup.string().email("Input must be valid email!").required("Email field is required!"),
+	})
+	.required();
 
 export default function Auth() {
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors, isSubmitting },
-  } = useForm<{ email: string }>({
-    resolver: yupResolver(loginSchema),
-  });
+	const {
+		register,
+		handleSubmit,
+		control,
+		formState: { errors, isSubmitting },
+	} = useForm<{ email: string }>({
+		resolver: yupResolver(loginSchema),
+	});
 
-  const [sentStatus, setSentStatus] = useState<"Sent" | "Error" | undefined>();
-  const [emailData, setEmailData] =
-    useState<{ email?: string; emailRequired?: boolean }>();
+	const [sentStatus, setSentStatus] = useState<"Sent" | "Error" | undefined>();
 
-  const handleLogin = async (data: any) => {
-    console.log({ data });
-    try {
-      // const { error } = await supabase.auth.signIn({ email: emailData?.email });
-      setSentStatus("Sent");
-    } catch (error) {
-      setSentStatus("Error");
-    }
-  };
+	const onSubmit = async ({ email }: { email: string }) => {
+		try {
+			await supabase.auth.signIn({ email });
+			setSentStatus("Sent");
+		} catch (error) {
+			setSentStatus("Error");
+		}
+	};
+	return (
+		<Flex width="100%" height="100vh" justifyContent="center" alignItems="center">
+			<Flex flexDirection="column" gap="1rem" maxWidth="370px">
+				<Heading as="h1" size="4xl">
+					Join Livres!
+				</Heading>
+				<Text fontSize="3xl">Join via magic link with your email below.</Text>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Controller
+						name="email"
+						control={control}
+						render={({ field }) => (
+							<Input
+								{...register("email")}
+								size="lg"
+								fontSize="2rem"
+								pl="0.5rem"
+								_placeholder={{
+									fontSize: "2rem",
+								}}
+								variant="outline"
+								type="email"
+								placeholder="Email address"
+								value={field.value || ""}
+								onChange={field.onChange}
+							/>
+						)}
+					/>
+					<Text fontSize="2rem" color="red.500" pl="0.5rem">
+						{errors.email?.message}
+					</Text>
 
-  return (
-    <Flex
-      width="85%"
-      height="100vh"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Flex flexDirection="column" gap="1rem">
-        <Heading as="h1" size="4xl">
-          Join Livres!
-        </Heading>
-        <Text fontSize="3xl">Join via magic link with your email below.</Text>
-        <FormControl
-          isInvalid={emailData?.emailRequired}
-          onSubmit={handleSubmit(handleLogin)}
-        >
-          <Controller
-            name="email"
-            control={control}
-            render={({ field }) => (
-              <Input
-                {...register("email")}
-                size="lg"
-                fontSize="2rem"
-                _placeholder={{
-                  fontSize: "2rem",
-                }}
-                variant="outline"
-                type="email"
-                placeholder="Email address"
-                value={field.value}
-                onChange={field.onChange}
-              />
-            )}
-          />
-          {errors.email && (
-            <FormErrorMessage fontSize="2rem">
-              Email is required.
-            </FormErrorMessage>
-          )}
-        </FormControl>
+					<Button
+						mt="1rem"
+						color="#3D2C8D"
+						size="lg"
+						type="submit"
+						fontSize="2rem"
+						variant="solid"
+						disabled={isSubmitting}
+						backgroundColor="gray.100"
+						_hover={{
+							backgroundColor: "gray.300",
+						}}
+					>
+						<Box>{isSubmitting ? "Loading" : "Send magic link"}</Box>
+					</Button>
+				</form>
 
-        <Button
-          color="#3D2C8D"
-          size="lg"
-          type="submit"
-          fontSize="2rem"
-          variant="solid"
-          disabled={isSubmitting}
-          backgroundColor="gray.100"
-          _hover={{
-            backgroundColor: "gray.300",
-          }}
-          onClick={handleLogin}
-          isSubmitting={isSubmitting}
-        >
-          <Box>{isSubmitting ? "Loading" : "Send magic link"}</Box>
-        </Button>
-        <Text fontSize="2rem" color="green.300">
-          {sentStatus === "Sent" && "Check your email for the login link!"}
-        </Text>
-        <Text fontSize="2rem" color="red.500">
-          {sentStatus === "Error" && "Something went wrong!"}
-        </Text>
-      </Flex>
-    </Flex>
-  );
+				<Text fontSize="2rem" color="green.300">
+					{sentStatus === "Sent" && "Check your email for the login link!"}
+				</Text>
+				<Text fontSize="2rem" color="red.500">
+					{sentStatus === "Error" && "Something went wrong!"}
+				</Text>
+			</Flex>
+		</Flex>
+	);
 }
