1fda34eeb8ab28a79b656dc62a9392f95a8d9eac
ogzhanolguncu <ogzhan11@gmail.com>
2022-01-23 16:27:16 +0300

    Remove unused imports

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,39 +3,38 @@ import { Flex, VStack, Text, Box } from "@chakra-ui/react";
 import { Book } from "@prisma/client";
 
 type Props = {
-	book: Book;
+  book: Book;
 };
 
 const cardBreakPoints = ["100px", "240px", "240px", "300px"];
 const fontSizeBreakPoints = ["md", "2xl", "2xl", "3xl"];
 const bookNameBreakPoints = ["md", "4xl", "4xl", "5xl"];
-const addButtonPaddingBreakPoints = ["0.33rem", "0.66rem", "1rem", "1rem"];
 
 const Card: FC<Props> = ({ book }) => {
-	return (
-		<Flex
-			flexDirection="column"
-			key={book.id}
-			alignItems={["center", "center", "flex-start", "flex-start"]}
-		>
-			<Box position="relative" width={cardBreakPoints} height={cardBreakPoints}>
-				<Flex
-					transition="all 0.5s ease"
-					boxShadow="20px 38px 34px -26px rgb(0 0 0 / 20%)"
-					borderRadius="md"
-					width={cardBreakPoints}
-					backgroundColor="#3d2c8d8c"
-					border="3px solid #e76bde"
-					height={cardBreakPoints}
-					alt="cover"
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Text fontSize={bookNameBreakPoints} textAlign="center">
-						{book.name}
-					</Text>
-				</Flex>
-				{/* {book.status === 0 && (
+  return (
+    <Flex
+      flexDirection="column"
+      key={book.id}
+      alignItems={["center", "center", "flex-start", "flex-start"]}
+    >
+      <Box position="relative" width={cardBreakPoints} height={cardBreakPoints}>
+        <Flex
+          transition="all 0.5s ease"
+          boxShadow="20px 38px 34px -26px rgb(0 0 0 / 20%)"
+          borderRadius="md"
+          width={cardBreakPoints}
+          backgroundColor="#3d2c8d8c"
+          border="3px solid #e76bde"
+          height={cardBreakPoints}
+          alt="cover"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text fontSize={bookNameBreakPoints} textAlign="center">
+            {book.name}
+          </Text>
+        </Flex>
+        {/* {book.status === 0 && (
 					<Text
 						position="absolute"
 						bottom="0"
@@ -51,15 +50,17 @@ const Card: FC<Props> = ({ book }) => {
 						Add To Lib
 					</Text>
 				)} */}
-			</Box>
+      </Box>
 
-			<VStack align="flex-start" px="0.5rem" mt="1.5rem" maxWidth="300px">
-				<Text fontSize={fontSizeBreakPoints}>Author: {book.author}</Text>
-				<Text fontSize={fontSizeBreakPoints}>Page Number: {book.pageNumber}</Text>
-				{/* <Text fontSize={fontSizeBreakPoints}>Completion: {book.status === 0 ? "✔️" : "⌛"}</Text> */}
-			</VStack>
-		</Flex>
-	);
+      <VStack align="flex-start" px="0.5rem" mt="1.5rem" maxWidth="300px">
+        <Text fontSize={fontSizeBreakPoints}>Author: {book.author}</Text>
+        <Text fontSize={fontSizeBreakPoints}>
+          Page Number: {book.pageNumber}
+        </Text>
+        {/* <Text fontSize={fontSizeBreakPoints}>Completion: {book.status === 0 ? "✔️" : "⌛"}</Text> */}
+      </VStack>
+    </Flex>
+  );
 };
 
 export default Card;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,62 +1,13 @@
 import { Center, ChakraProvider } from "@chakra-ui/react";
 import theme from "@styles/theme";
-import { supabase } from "lib/supabaseClient";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 
 import type { AppProps } from "next/app";
-import type { AuthChangeEvent, Session } from "@supabase/supabase-js";
-import Sidebar from '@components/Sidebar';
+import Sidebar from "@components/Sidebar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [authenticatedState, setAuthenticatedState] =
-    useState("not-authenticated");
-
-  useEffect(() => {
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        console.log({ event, session });
-        handleAuthChange(event, session);
-        if (event === "SIGNED_IN") {
-          setAuthenticatedState("authenticated");
-          router.push("/profile");
-        }
-        if (event === "SIGNED_OUT") {
-          setAuthenticatedState("not-authenticated");
-        }
-      }
-    );
-    checkUser();
-    return () => {
-      authListener?.unsubscribe();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const checkUser = async () => {
-    const user = supabase.auth.user();
-    if (user) {
-      setAuthenticatedState("authenticated");
-    }
-  };
-
-  async function handleAuthChange(
-    event: AuthChangeEvent,
-    session: Session | null
-  ) {
-    /* sets and removes the Supabase cookie */
-    await fetch("/api/auth", {
-      method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
-      credentials: "same-origin",
-      body: JSON.stringify({ event, session }),
-    });
-  }
-
   return (
     <ChakraProvider theme={theme}>
-      <Center alignItems="flex-start" pos="relative">
+      <Center alignItems="flex-start">
         <Sidebar />
         <Component {...pageProps} />
       </Center>
