7b69e95b38bb654ebf6c8442947638f8c975b4ca
Serhat Genc <serhat.genc@fireflyon.com>
2022-01-24 16:30:24 +0300

    redirects handled and framer motion added to navItem

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import { Tooltip, Button } from '@chakra-ui/react';
 import type { IconType } from 'react-icons';
+import { motion } from 'framer-motion';
 
 type Props = {
   icon: IconType;
@@ -10,6 +11,14 @@ type Props = {
   to?: string;
   onClick?: () => void;
   authStatus?: boolean;
+};
+
+const container = {
+  hidden: { opacity: 0, x: -100 },
+  show: {
+    opacity: 1,
+    x: 0,
+  },
 };
 
 const NavItem = ({
@@ -21,24 +30,25 @@ const NavItem = ({
   authStatus,
 }: Props) => {
   return authStatus ? (
-    <Link href={to ?? '#'} passHref>
-      <div>
-        <Tooltip label={label} placement="right" fontSize="1.5rem" hasArrow>
-          <Button
-            my="1rem"
-            aria-label={ariaLabel}
-            leftIcon={<Icon />}
-            bg="transparent"
-            fontSize={['2rem', '2rem', '2.5rem', '3rem']}
-            _hover={{ background: 'transparent', color: '#e76bde' }}
-            _focus={{ background: 'transparent', outline: 'none' }}
-            _active={{ background: 'transparent' }}
-            onClick={onClick}
-            transition="all 0.5s ease"
-          />
-        </Tooltip>
-      </div>
-    </Link>
+    <motion.div variants={container} initial="hidden" animate="show">
+      <Link href={to ?? '#'} passHref>
+        <div>
+          <Tooltip label={label} placement="right" fontSize="1.5rem" hasArrow>
+            <Button
+              my="1rem"
+              aria-label={ariaLabel}
+              leftIcon={<Icon />}
+              bg="transparent"
+              fontSize={['2rem', '2rem', '2.5rem', '3rem']}
+              _hover={{ background: 'transparent', color: '#e76bde' }}
+              _focus={{ background: 'transparent', outline: 'none' }}
+              _active={{ background: 'transparent' }}
+              onClick={onClick}
+            />
+          </Tooltip>
+        </div>
+      </Link>
+    </motion.div>
   ) : null;
 };
 
